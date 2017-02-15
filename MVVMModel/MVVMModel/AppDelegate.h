ab9cef59039dd130b4773879aff3457700ece625
RXL <rxl@192.168.1.125>
2017-02-15 15:14:54 +0800

    初始化

---
+++ MVVMModel/MVVMModel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVVMModel
+//
+//  Created by RXL on 17/2/15.
+//  Copyright © 2017年 RXL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MVVMModel/MVVMModel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MVVMModel
+//
+//  Created by RXL on 17/2/15.
+//  Copyright © 2017年 RXL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
